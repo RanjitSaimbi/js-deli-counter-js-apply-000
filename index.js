0868c40f25ec2d1b0dfa817eb4acf068860f6329
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-02-04 10:17:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-function takeANumber(currentLine, newPerson) {
+function takeANumber(currentLine) {
   var newPersonNumber = currentLine.length + 1;
   currentLine.push(newPerson);
   return `Welcome, ${newPerson}. You are number ${newPersonNumber} in line.`;
+  
+  // return a number that represents  number on a ticket
   
 }
 
